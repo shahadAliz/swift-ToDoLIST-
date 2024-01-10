a4a97ed0a1799e9791ef34152d3f27358a18fbb5
SHAHAD ALI <cs.shahadz@gmail.com>
2024-01-11 00:59:47 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by شهد علي on 29/06/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
